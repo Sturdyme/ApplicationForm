b65b8650b7837baca69bbeb38ea5913b2ef76e33
Sturdyme <emmanuel08135672350@gmail.com>
2026-03-02 16:01:41 -0500

    target

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://applicationform-backend-3.onrender.com',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
